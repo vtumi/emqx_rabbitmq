7cdb61a63799d5285fe5278bffc729001b266685
Tumi <121513342@qq.com>
2020-03-04 21:04:02 +0800

    Initial commit

--- src/emqx_rabbitmq.erl
+++ src/emqx_rabbitmq.erl
@@ -11,25 +11,33 @@
 
 on_message_publish(Message = #message{topic = <<"$SYS/", _/binary>>}, _Env) ->
   {ok, Message};
-on_message_publish(Message = #message{topic = Topic, flags = #{retain := Retain}}, ExchangeName) ->
+on_message_publish(Message = #message{id = Id, topic = Topic, qos = Qos, payload = Payload, timestamp = Timestamp, headers = #{username := Username, peername := {Peerhost, _}}, flags = #{retain := Retain}}, ExchangeName) ->
   if
     Qos > 0 ->
-      Username = case maps:find(username, Message#message.headers) of
-        {ok, Value} -> Value;
-        _ -> undefined
-      end,
-      Params = #{ client_id => Message#message.from
-                , username => Username
-                , topic => Message#message.topic
-                , qos => Message#message.qos
-                , retain => Retain
-                , payload => Message#message.payload
-                , publish_at => Message#message.timestamp
+      Params = #{ mid => emqx_guid:to_hexstr(Id)
+                , topic => Topic
+                , sender => Username
+                , node => atom_to_binary(node(), utf8)
+                , ipaddr => iolist_to_binary(inet_parse:ntoa(Peerhost))
+                , qos => Qos
+                , retain => format_retain(Retain)
+                , payload => Payload
+                , create_at => format_timestamp(Timestamp)
       },
       emqx_rabbitmq_cli:publish(ExchangeName, emqx_json:encode(Params), <<"message.publish">>);
     true ->
       true
   end,
   {ok, Message}.
+
+format_retain(Retain) ->
+  case Retain of
+    true -> 1;
+    false -> 0
+  end.
+
+format_timestamp(TS) ->
+  {{Year, Month, Day}, {Hour, Minute, Second}} = calendar:now_to_local_time(TS),
+  lists:flatten(io_lib:format("~B-~2.10.0B-~2.10.0B ~2.10.0B:~2.10.0B:~2.10.0B", [Year, Month, Day, Hour, Minute, Second])).
 
 description() -> "Bridge of RabbitMQ".
