66ccffc467f48002ed31626d72148df250f06e9f
Tumi <121513342@qq.com>
2020-03-04 20:17:56 +0800

    Initial commit

--- src/emqx_rabbitmq.erl
+++ src/emqx_rabbitmq.erl
@@ -1,30 +1,35 @@
 -module(emqx_rabbitmq).
 
--include_lib("emqx/include/emqx.hrl").
+-include("emqx_rabbitmq.hrl").
 
--export([on_message_publish/2
-]).
+-include_lib("emqx/include/emqx.hrl").
 
 -import(emqx_rabbitmq_cli, [ensure_exchange/1, publish/3]).
 
--include("emqx_rabbitmq.hrl").
+-export([description/0]).
+-export([on_message_publish/2]).
 
 on_message_publish(Message = #message{topic = <<"$SYS/", _/binary>>}, _Env) ->
   {ok, Message};
 on_message_publish(Message = #message{topic = Topic, flags = #{retain := Retain}}, ExchangeName) ->
-  Username = case maps:find(username, Message#message.headers) of
-    {ok, Value} -> Value;
-    _ -> undefined
+  if
+    Qos > 0 ->
+      Username = case maps:find(username, Message#message.headers) of
+        {ok, Value} -> Value;
+        _ -> undefined
+      end,
+      Params = #{ client_id => Message#message.from
+                , username => Username
+                , topic => Message#message.topic
+                , qos => Message#message.qos
+                , retain => Retain
+                , payload => Message#message.payload
+                , publish_at => Message#message.timestamp
+      },
+      emqx_rabbitmq_cli:publish(ExchangeName, emqx_json:encode(Params), <<"message.publish">>);
+    true ->
+      true
   end,
-  Params = #{ client_id => Message#message.from
-            , username => Username
-            , topic => Message#message.topic
-            , qos => Message#message.qos
-            , retain => Retain
-            , payload => Message#message.payload
-            , publish_at => Message#message.timestamp
-  },
-  emqx_rabbitmq_cli:publish(ExchangeName, emqx_json:encode(Params), <<"message.publish">>),
   {ok, Message}.
 
 description() -> "Bridge of RabbitMQ".
--- src/emqx_rabbitmq_sup.erl
+++ src/emqx_rabbitmq_sup.erl
@@ -3,7 +3,6 @@
 -behaviour(supervisor).
 
 -export([start_link/0]).
-
 -export([init/1]).
 
 -include("emqx_rabbitmq.hrl").
